24b457bd8132e26fcd8746b5060fdd5f5cd4eefd
Anoopa <anoopa24@gmail.com>
2020-06-30 19:40:42 -0400

    initial commit

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+#list
+anoopalist = ["Jeff", "Olson", 28, "Brown", "Brown", 5.8, 180, 42, "jolson@upperlinecode.com", "(615)-555-5555"]
+print(anoopalist[3])
+#dictionary
+anoopadictionary = {"firstname": "Jeff",
+    "lastname": "Olson",
+    "age": 28,
+    "eyecolor": "Brown",
+    "haircolor": "Brown",
+    "height": 5.8,
+    "weight": 180,
+    "lucky_number": 42,
+    "email": "jolson@upperlinecode.com",
+    "phone": "(615)-555-5555"
+}
+print(anoopadictionary)
